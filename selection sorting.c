1a244d83041b02869d399f323a3be4eb43c07a6b
shabinvs <shabinvs64@gmail.com>
2024-12-06 00:31:21 +0530

    Update selection sorting.c

--- selection sorting.c
+++ selection sorting.c
@@ -2,38 +2,36 @@
 
 int main()
 {
-    int size;
+	int size;
 
-    printf("Enter the size: ");
-    scanf("%d", &size);
+	printf("Enter the size: ");
+	scanf("%d", &size);
 
-    int arr[size];
+	int arr[size];
 
-    printf("Enter the elements: ");
-    for (int i = 0; i < size; i++) {
-        scanf("%d", &arr[i]);
-    }
+	printf("Enter the elements: ");
+	for (int i = 0; i < size; i++) {
+		scanf("%d", &arr[i]);
+	}
 
 
-    for (int i = 0; i < size - 1; i++) {
-        int sort = i; 
-        for (int j = i + 1; j < size; j++) {
-            if (arr[j] < arr[sort]) {
-                sort = j;
-            }
-        }
+	for (int i = 0; i < size - 1; i++) {
+		int sort = i;
+		for (int j = i + 1; j < size; j++) {
+			if (arr[j] < arr[sort]) {
+				sort = j;
+			}
+		}
 
-        if (sort != i) {
-            int temp = arr[sort];
-            arr[sort] = arr[i];
-            arr[i] = temp;
-        }
-    }
+		int temp = arr[sort];
+		arr[sort] = arr[i];
+		arr[i] = temp;
+	}
 
-    printf("After sorting: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
+	printf("After sorting: ");
+	for (int i = 0; i < size; i++) {
+		printf("%d ", arr[i]);
+	}
 
-    return 0;
+	return 0;
 }
